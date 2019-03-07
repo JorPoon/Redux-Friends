bc5476a9a808321baa1aea0caa52d10c388e4a41
Jor_Poon <jormpoon@gmail.com>
2019-03-07 17:50:59 -0500

    boilerplating

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,15 +1,24 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import User from './components/User';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () =>  {
     return (
-      <div className="App">
- 
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/user">Sign In</Link>
+          </nav>
+        <h1>Welcome to Friends</h1>
+            <Route path="/login" component={Login} />
+            <PrivateRoute exact path="/user" component={User} />
+        </div>
+      </Router>
     );
-  }
 }
 
 export default App;
--- friends/src/index.js
+++ friends/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
 
 
